c74346158dfe321f11f34ab19ebe128c633b6a35
josemedina1 <medinavera.jose@gmail.com>
2024-08-23 21:40:08 -0400

    cambio ruta settings

--- capstone-josemedina/wsgi.py
+++ capstone-josemedina/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone-josemedina.settings')
 
 app = get_wsgi_application()
